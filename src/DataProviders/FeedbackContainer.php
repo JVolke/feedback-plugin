dae42a3e391fa0efbc4436d1f8bbb53be5f03f73
Jens Volke <desire.add@gmail.com>
2021-10-15 09:21:53 +0200

    reuse option for allowguest from the config

--- src/DataProviders/FeedbackContainer.php
+++ src/DataProviders/FeedbackContainer.php
@@ -2,13 +2,19 @@
 
 namespace Feedback\DataProviders;
 
+use Plenty\Plugin\ConfigRepository;
 use Plenty\Plugin\Templates\Twig;
+use
 
 class FeedbackContainer
 {
     public function call(Twig $twig):string
     {
+        $configRepository = pluginApp(ConfigRepository::class);
 
-        return $twig->render('Feedback::DataProvider.FeedbackContainer');
+
+        return $twig->render('Feedback::DataProvider.FeedbackContainer', [
+            "allowGuest" => $configRepository->get("Feedback.allowGuestFeedbacks")
+        ]);
     }
 }
